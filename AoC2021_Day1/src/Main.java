713224a304ee39d7aebeaf5286d5746efce58b66
Jan Kressin <jan.kressin@googlemail.com>
2022-12-10 21:07:06 +0100

    day 1 part 2

--- AoC2021_Day1/src/Main.java
+++ AoC2021_Day1/src/Main.java
@@ -9,11 +9,19 @@ public class Main {
 
 		int sum = 0;
 
-		for (int i = 0; i < strings.size() - 1; i++) {
-			if (Integer.valueOf(strings.get(i + 1)) > Integer.valueOf(strings.get(i)))
+		for (int i = 0; i < strings.size() - 3; i++) {
+			int sumA = 0;
+			int sumB = 0;
+			
+			for(int j=0; j<3; j++) {
+				sumA += Integer.valueOf(strings.get(i+j));
+				sumB += Integer.valueOf(strings.get(i+j+1));
+			}
+			
+			if (sumB>sumA)
 				sum++;
 		}
-
+		
 		System.out.println(sum);
 	}
 }
